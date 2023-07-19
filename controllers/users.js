50f556208b33370c3ffdef5f6901f735eafa232d
Dana Kun <dana.liepinsh@gmail.com>
2023-07-19 11:23:30 +0200

    fix userUpdate

--- controllers/users.js
+++ controllers/users.js
@@ -14,6 +14,95 @@ const createUser = (req, res, next) => User.create(req.body)
     }
   });
 
+const getUsers = (req, res, next) => User.find({})
+  .orFail(() => {
+    throw new InternalServerError();
+  })
+  .then((users) => res.status(200).send(users))
+  .catch(() => {
+    next(new InternalServerError());
+  });
+
+const getUser = (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (!user) {
+        throw new NotFound('Пользователь по указанному id не найден');
+      } else {
+        res.status(200).send(user);
+      }
+    })
+    .catch((err) => {
+      if (err.name === 'CastError' || err.name === 'ValidationError') {
+        res.status(400).send({ message: 'Переданы некорректные данные' });
+      } else if (err instanceof NotFound) {
+        res.status(err.status).send({ message: err.message });
+      } else {
+        res.status(500).send({ message: 'Внутренняя ошибка сервера' });
+      }
+    });
+};
+
+const updateUser = (req, res) => {
+  const { name, about } = req.body;
+  return User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      if (!user) {
+        res.status(NotFound).send({ message: 'Пользователь по указанному id не найден' });
+        return;
+      }
+      res.status(200).send({ data: user });
+    })
+    .catch((error) => {
+      if (error.name === 'CastError' || error.name === 'ValidationError') {
+        res.status(BadRequest).send({ message: 'Переданы некорректные данные' });
+      } else {
+        res.status(InternalServerError).send({ message: 'Внутренняя ошибка сервера' });
+      }
+    });
+};
+
+const updateAvatar = (req, res, next) => {
+  const { avatar } = req.body;
+  return User
+    .findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .then((user) => res.send(user))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequest('При обновлении аватара переданы некорректные данные'));
+        return;
+      }
+      next(new InternalServerError());
+    });
+};
+
+module.exports = {
+  createUser, getUser, getUsers, updateUser, updateAvatar,
+};
+
+// const getUser = (req, res) => {
+//   const { userId } = req.params;
+
+//   User.findById(userId)
+//     .then((user) => {
+//       if (!user) {
+//         res.status(NotFound).send({ message: 'Пользователь не найден' });
+//         return;
+//       }
+//       res.status(200).send(user);
+//     })
+//     .catch((err) => {
+//       if (err.name === 'CastError' || err.name === 'ValidationError') {
+//         res.status(BadRequest).send({ message: 'Переданы некорректные данные' });
+//       } else {
+//         res.status(InternalServerError).send({ message: err.message });
+//       }
+//     });
+// };
 //  const createUser = (req, res) =>
 // return User
 //   .create(req.body)
@@ -76,93 +165,3 @@ const createUser = (req, res, next) => User.create(req.body)
 //       }
 //     });
 // };
-
-const getUser = (req, res) => {
-  User.findById(req.params.userId)
-    .then((user) => {
-      if (!user) {
-        throw new NotFound('Пользователь по указанному id не найден');
-      } else {
-        res.status(200).send(user);
-      }
-    })
-    .catch((err) => {
-      if (err.name === 'CastError' || err.name === 'ValidationError') {
-        res.status(400).send({ message: 'Переданы некорректные данные' });
-      } else if (err instanceof NotFound) {
-        res.status(err.status).send({ message: err.message });
-      } else {
-        res.status(500).send({ message: 'Внутренняя ошибка сервера' });
-      }
-    });
-};
-
-// const getUser = (req, res) => {
-//   const { userId } = req.params;
-
-//   User.findById(userId)
-//     .then((user) => {
-//       if (!user) {
-//         res.status(NotFound).send({ message: 'Пользователь не найден' });
-//         return;
-//       }
-//       res.status(200).send(user);
-//     })
-//     .catch((err) => {
-//       if (err.name === 'CastError' || err.name === 'ValidationError') {
-//         res.status(BadRequest).send({ message: 'Переданы некорректные данные' });
-//       } else {
-//         res.status(InternalServerError).send({ message: err.message });
-//       }
-//     });
-// };
-
-const getUsers = (req, res, next) => User.find({})
-  .orFail(() => {
-    throw new InternalServerError();
-  })
-  .then((users) => res.status(200).send(users))
-  .catch(() => {
-    next(new InternalServerError());
-  });
-
-const updateUser = (req, res) => {
-  const { name, about } = req.body;
-  return User.findByIdAndUpdate(
-    req.user._id,
-    { name, about },
-    { new: true, runValidators: true },
-  )
-    .then((user) => {
-      if (!user) {
-        res.status(NotFound).send({ message: 'Пользователь по указанному id не найден' });
-        return;
-      }
-      res.status(200).send({ data: user });
-    })
-    .catch((error) => {
-      if (error.name === 'CastError' || error.name === 'ValidationError') {
-        res.status(BadRequest).send({ message: 'Переданы некорректные данные' });
-      } else {
-        res.status(InternalServerError).send({ message: 'Внутренняя ошибка сервера' });
-      }
-    });
-};
-
-const updateAvatar = (req, res, next) => {
-  const { avatar } = req.body;
-  return User
-    .findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-    .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        next(new BadRequest('При обновлении аватара переданы некорректные данные'));
-        return;
-      }
-      next(new InternalServerError());
-    });
-};
-
-module.exports = {
-  createUser, getUser, getUsers, updateUser, updateAvatar,
-};
--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,9 @@ router.get('/users', getUsers);
 router.get('/users/:userId', getUser);
 // router.get('/:id', getUser);
 router.post('/users', createUser);
-router.patch('/users/me', updateUser);
-router.patch('/users/me/avatar', updateAvatar);
+// router.patch('/users/me', updateUser);
+// router.patch('/users/me/avatar', updateAvatar);
+router.patch('/me', updateUser);
+router.patch('me/avatar', updateAvatar);
 
 module.exports = router;
