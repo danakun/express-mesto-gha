89b6acf1ccf861d1c6397be8c511a0d856f7af51
Dana Kun <dana.liepinsh@gmail.com>
2023-07-19 14:07:00 +0200

    create cards controller

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 // const Router = require('./routes/index');
 const usersRouter = require('./routes/users');
 // const cardsRouter = require('./routes/cards');
+// const { NotFound } = require('./errors/NotFound');
 
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
@@ -14,12 +15,18 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', usersRouter);
+// app.use('/users', usersRouter);
 app.use((req, res, next) => {
   req.user = {
     _id: '6411a0175f26d1ed6a973834',
   };
   next();
 });
+//   app.use('*', (req, res, next) => next(
+//     res.status(NotFound).send({ message: 'Страница не найдена' }),
+//   ));
+//   next();
+// });
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
--- controllers/cards.js
+++ controllers/cards.js
@@ -1,33 +1,98 @@
-const Card = require('../models/card');
-
-const getCards = (req, res) => {
-
-};
+const card = require('../models/card');
+const {
+  NotFound,
+  BadRequest,
+  InternalServerError,
+} = require('./users');
 
 const createCard = (req, res) => {
-  console.log(req.user._id);
+  const { name, link } = req.body;
+  return card
+    .create({ name, link, owner: req.user._id })
+    .then((crd) => res.status(201).send(crd))
+    .catch((error) => {
+      if (error.name === 'ValidationError') {
+        res.status(BadRequest).send({
+          message: 'Переданы некорректные данные при создании карточки',
+        });
+      } else {
+        res.status(InternalServerError).send({ message: 'Ошибка сервера' });
+      }
+    });
 };
 
-const deleteCard = (req, res) => {
+const deleteCard = (req, res) => card
+  .findByIdAndRemove(req.params.cardId)
+  .then((crd) => {
+    if (!crd) {
+      res.status(NotFound).send({ message: 'Карточка не найдена' });
+      return;
+    }
+    res.send({ data: crd });
+  })
+  .catch((error) => {
+    if (error.name === 'CastError') {
+      res
+        .status(BadRequest)
+        .send({ message: 'Переданы некорректные данные' });
+    } else {
+      res.status(InternalServerError).send({ message: 'Ошибка сервера' });
+    }
+  });
 
-};
+const getCards = (req, res) => card
+  .find({})
+  .then((cards) => res.status(200).send(cards))
+  .catch((error) => {
+    res.status(BadRequest).send({ message: `Некорректные данные: ${error}` });
+  });
 
-const likeCard = (req, res) => {
-  Card.findByIdAndUpdate(
+const likeCard = (req, res) => card
+  .findByIdAndUpdate(
     req.params.cardId,
-    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { $addToSet: { likes: req.user._id } },
     { new: true },
-  );
-};
+  )
+  .then((crd) => {
+    if (!crd) {
+      res.status(NotFound).send({ message: 'Карточка не найдена' });
+      return;
+    }
+    res.send({ data: crd });
+  })
+  .catch((error) => {
+    if (error.name === 'CastError') {
+      res.status(BadRequest).send({ message: 'Переданы некорректные данные' });
+    } else {
+      res.status(InternalServerError).send({ message: 'Ошибка сервера' });
+    }
+  });
 
-const dislikeCard = (req, res) => {
-  Card.findByIdAndUpdate(
+const dislikeCard = (req, res) => card
+  .findByIdAndUpdate(
     req.params.cardId,
-    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { $pull: { likes: req.user._id } },
     { new: true },
-  );
-};
+  )
+  .then((crd) => {
+    if (!crd) {
+      res.status(NotFound).send({ message: 'Карточка не найдена' });
+      return;
+    }
+    res.send({ data: crd });
+  })
+  .catch((error) => {
+    if (error.name === 'CastError') {
+      res.status(BadRequest).send({ message: `Пользователь не найден: ${error}` });
+    } else {
+      res.status(InternalServerError).send({ message: 'Ошибка сервера' });
+    }
+  });
 
 module.exports = {
-  getCards, createCard, deleteCard, likeCard, dislikeCard,
+  createCard, deleteCard, getCards, likeCard, dislikeCard,
 };
+
+// module.exports.createCard = (req, res) => {
+//   console.log(req.user._id); // _id станет доступен
+// };
